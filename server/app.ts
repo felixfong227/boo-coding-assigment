8e603798e89784fd179072b8fc10a16e08018168
FeliΞ <moongod101@hotmail.com>
2022-11-18 23:33:13 +0000

    add body parser and api routers

--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,33 @@
 'use strict';
 
 import express from 'express';
+import mongoose from 'mongoose';
 import profileRouter from './routes/profile';
-const app = express();
-const port =  process.env.PORT || 3000;
+import { mongoDB } from './utils/db';
+import userSeed from './seed/users';
+import bodyParser from 'body-parser';
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// routes
-app.use('/', profileRouter);
-
-// start server
-app.listen(port);
-console.log('Express started. Listening on %s', port);
+async function main() {
+    const app = express();
+    const port =  process.env.PORT || 3000;
+	
+    // set the view engine to ejs
+    app.set('view engine', 'ejs');
+	
+    const db = await mongoDB();
+    const dbUrl = db.getUri();
+    await mongoose.connect(dbUrl);
+    console.info(`Connected to ${dbUrl}`);
+    
+    await userSeed();
+    
+    app.use(bodyParser.json());
+    
+    // routes
+    app.use('/', profileRouter);
+	
+    // start server
+    app.listen(port);
+    console.log('Express started. Listening on %s', port);
+}
+main();
