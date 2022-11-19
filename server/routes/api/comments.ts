7cf25e6d299b63f176b7bf527305f1b95e7dc040
FeliΞ <moongod101@hotmail.com>
2022-11-19 00:49:36 +0000

    create API route for fetching comments by uid

--- server/routes/api/comments.ts
+++ server/routes/api/comments.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { ZodError } from 'zod';
+import { z, ZodError } from 'zod';
 import Comment from '../../model/comments';
 import User from '../../model/user';
 const router = express.Router();
@@ -16,6 +16,28 @@ router.post('/', async function(req, res) {
         return res.status(400).json(newComment);
     }
     return res.status(201).json(newComment);
+});
+
+router.get('/:uid', async function(req, res) {
+    const user = new User();
+    const comment = new Comment(
+        user,
+    );
+    const uidSchema = z.number().positive();
+    const uid = uidSchema.safeParse(
+        parseInt(req.params.uid)
+    );
+    if(!uid.success) {
+        res.status(400).send(uid.error.message);
+        return;
+    }
+    const isUserExists = await user.checkUserExists(uid.data);
+    if(!isUserExists) {
+        res.status(404).send('User not found');
+        return;
+    }
+    const commentsBelongToUser = await comment.getCommentsByUserId(uid.data);
+    return res.status(200).json(commentsBelongToUser);
 });
 
 export default router;
