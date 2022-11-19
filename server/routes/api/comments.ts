8c8b395bb898b7294377f03a3635ef771021ccbd
FeliΞ <moongod101@hotmail.com>
2022-11-19 01:34:12 +0000

    create API route for liking a comment

--- server/routes/api/comments.ts
+++ server/routes/api/comments.ts
@@ -18,6 +18,47 @@ router.post('/', async function(req, res) {
     return res.status(201).json(newComment);
 });
 
+router.post('/likes/:commentId', async function(req, res) {
+    const user = new User();
+    const comment = new Comment(
+        user,
+    );
+    const commentIdSchema = z.string();
+    const commentId = commentIdSchema.safeParse(req.params.commentId);
+    const payloadSchema = z.object({
+        uid: z.number(),
+    });
+    if(!commentId.success) {
+        return res.status(400).json(commentId.error);
+    }
+    const payload = payloadSchema.safeParse(req.body);
+    if(!payload.success) {
+        return res.status(400).json(payload.error);
+    }
+    const newLike = await comment.likeAComment(commentId.data, payload.data.uid);
+    if(newLike instanceof ZodError) {
+        return res.status(400).json(newLike);
+    }
+    return res.json(newLike);
+});
+
+router.get('/likes/:commentId', async function(req, res) {
+    const user = new User();
+    const comment = new Comment(
+        user,
+    );
+    const commentIdSchema = z.string();
+    const commentId = commentIdSchema.safeParse(req.params.commentId);
+    if(!commentId.success) {
+        return res.status(400).json(commentId.error);
+    }
+    const likes = await comment.getLikesByCommentId(commentId.data);
+    if(likes instanceof ZodError) {
+        return res.status(400).json(likes);
+    }
+    return res.json(likes);
+});
+
 router.get('/:uid', async function(req, res) {
     const user = new User();
     const comment = new Comment(
